5bfac2d5f422ed24c086fa3d4c17526f1cbb981d
isaac2206 <isaac-lopes@hotmail.com>
2021-02-11 17:55:05 -0300

    11fev

---
+++ src/main/java/br/com/isidrocorp/eventdash/dao/EquipamentoDAO.java
@@ -0,0 +1,13 @@
+package br.com.isidrocorp.eventdash.dao;
+
+import java.util.ArrayList;
+
+import org.springframework.data.repository.CrudRepository;
+import br.com.isidrocorp.eventdash.model.Equipamento;
+
+
+
+public interface EquipamentoDAO extends CrudRepository<Equipamento, Integer>{
+	public ArrayList<Equipamento> findAllByHostname (String hostname);
+
+}
